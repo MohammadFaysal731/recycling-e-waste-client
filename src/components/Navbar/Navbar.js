91ef586ee7f50ced866c66209c1329b450be7529
MohammadFaysal731 <www.faysal731@gmail.com>
2023-04-21 09:07:53 +0600

    add team members

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { AiOutlineBars } from "react-icons/ai";
 import { Link, NavLink } from 'react-router-dom';
 import { navbarData } from './navbarData';
-
 
 
 const Navbar = () => {
@@ -11,20 +11,7 @@ const Navbar = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+             <AiOutlineBars className='text-primary text-2xl'/>
             </label>
             <ul
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Team = () => {
+  const [members, setMembers]=useState([]);
+  useEffect(()=>{
+    fetch("teamData.json")
+      .then((res) => res.json())
+      .then((data) => setMembers(data));
+  },[])
   return (
-    <div className="max-w-[1440px] mx-auto">
-      <h1>Team</h1>
+    <div className="max-w-[1440px] mx-auto p-10">
+      <h2 className="text-xl md:text-2xl font-bold text-primary text-center m-5">
+        Meet Our Team Members
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {members?.map(({ name, image }, index) => (
+          <div className="">
+            <div class="avatar">
+            <div class=" mask mask-hexagon">
+              <img src={image} alt="team-members-images" className="w-full" />
+            </div>
+          </div>
+          <p className='text-xl text-primary text-center font-bold'>{name}</p> 
+          </div>
+         
+        ))}
+      </div>
     </div>
   );
 };
